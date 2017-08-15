893fcdaf3ccbace11b9aa26e790ac13c7e8ddefc
Nikki Jedic <nikki.jedic@gmail.com>
2017-08-15 15:23:06 -0400

    Jumbotron Animation Complete

--- assets/javascript/animate.js
+++ assets/javascript/animate.js
@@ -1,6 +1,40 @@
-// // On Document ready
-// $(document).ready(function(){
+// ++++++FUNCTIONS+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-// 	// .addClass to make name come in 
-// 	$("#name").addClass("animated fadeInLeft");
-// })
+// Function to render Name
+function showText(target, message, index, interval) {
+	var deferred = $.Deferred();
+
+  if (index < message.length) { 
+    $(target).append(message[index++]); 
+    setTimeout(function(){ 
+    	showText(target, message, index, interval);
+    	deferred.resolve(); 
+    }, interval); 
+  } 
+  return deferred.promise();
+}
+
+
+
+
+// ++++++WEBSITE FUNCTIONALITY+++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// On Document Ready...
+$(document).ready(function(){
+	// Add class to fadeIn Profile Picture,
+	$("#profilePicture").addClass("animated fadeIn delay0s9ms");
+	// Call function to render Name,
+	showText("#name", "Nikki Jedic", 0, 300);
+	// Add class to fadeIn Description
+	$("#description").addClass("animated fadeIn delay2s9ms");
+
+});
+
+
+
+
+
+	
+ 
